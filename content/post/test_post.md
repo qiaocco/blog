a66ce3c1aae503d81b335017a954ec64071e1d04
qiaocc <jasonqiao36@gmail.com>
2021-02-04 14:19:08 +0800

    debug workflow

---
+++ content/post/test_post.md
@@ -0,0 +1,7 @@
+---
+title: "Test_post"
+date: 2021-02-04T14:15:22+08:00
+draft: false
+---
+
+# 测试
--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,6 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mBuilding...\033[0m\n"
-
-hugo -D
-
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
